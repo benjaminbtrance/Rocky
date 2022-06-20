42a157b4c979ad712cfb2a03a2df4f21cf45008f
benjaminbtrance <benjaminb.tran@gmail.com>
2022-06-20 03:07:35 -0500

    Client-side Validations and Category Update/Delete

--- Rocky/Controllers/CategoryController.cs
+++ Rocky/Controllers/CategoryController.cs
@@ -2,6 +2,7 @@
 using Rocky.Data;
 using Rocky.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Rocky.Controllers
 {
@@ -18,17 +19,84 @@ namespace Rocky.Controllers
             IEnumerable<Category> objList = _db.Category;
             return View(objList);
         }
+
         //Get - CREATE
-        public IActionResult Create() 
-        {       
+        public IActionResult Create()
+        {
             return View();
         }
+
         //POST - CREATE
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category obj)
         {
-            _db.Category.Add(obj);
+            //server side validation 
+            if (ModelState.IsValid)
+            {
+                _db.Category.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //Get - EDIT
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        //POST - EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            //server side validation 
+            if (ModelState.IsValid)
+            {
+                _db.Category.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //Get - DELETE
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        //POST - DELETE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Category.Find(id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            _db.Category.Remove(obj);
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
